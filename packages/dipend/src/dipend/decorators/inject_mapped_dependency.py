0c68a2238d33cb3e74aaead157fcf1419d4b0e8e
saulova <9355603+saulova@users.noreply.github.com>
2025-05-16 18:37:30 +0000

    fix: change qualifier token type

--- packages/dipend/src/dipend/decorators/inject_mapped_dependency.py
+++ packages/dipend/src/dipend/decorators/inject_mapped_dependency.py
@@ -4,7 +4,7 @@ from builtins import __dict__ as builtins_dict
 from ..exceptions.decorator_exception import DecoratorException
 
 
-def inject_mapped_dependency(constructor_index: int, qualifier_tokens: list[Any]):
+def inject_mapped_dependency(constructor_index: int, qualifier_token: Any):
     def decorator(cls: Callable):
         is_class = isclass(cls) and hasattr(cls, "__dict__") and cls not in builtins_dict.values()
 
@@ -14,13 +14,10 @@ def inject_mapped_dependency(constructor_index: int, qualifier_tokens: list[Any]
         if not isinstance(constructor_index, int):
             raise DecoratorException("constructor_index must be an int.")
 
-        if not isinstance(qualifier_tokens, list):
-            raise DecoratorException("qualifier_tokens must be a list.")
-
         if not hasattr(cls, "__di_mapped_dependency"):
             setattr(cls, "__di_mapped_dependency", {})
 
-        cls.__di_mapped_dependency[constructor_index] = qualifier_tokens
+        cls.__di_mapped_dependency[constructor_index] = [qualifier_token]
 
         return cls
 
--- tests/dipend_unit_tests/decorators/test_inject_mapped_dependency.py
+++ tests/dipend_unit_tests/decorators/test_inject_mapped_dependency.py
@@ -10,38 +10,28 @@ class TestInjectMappedDependency:
         class ValidClass:
             pass
 
-        decorated_class = inject_mapped_dependency(0, ["token1", "token2"])(ValidClass)
+        decorated_class = inject_mapped_dependency(0, "token1")(ValidClass)
 
         assert hasattr(decorated_class, "__di_mapped_dependency")
-        assert getattr(decorated_class, "__di_mapped_dependency")[0] == [
-            "token1",
-            "token2",
-        ]
+        assert getattr(decorated_class, "__di_mapped_dependency")[0] == ["token1"]
 
     def test_invalid_decorator_on_non_class(self):
         with pytest.raises(DecoratorException, match="inject_mapped_dependency is a class decorator."):
-            inject_mapped_dependency(0, ["token1"])(lambda x: x)
+            inject_mapped_dependency(0, "token1")(lambda x: x)
 
     def test_constructor_index_not_int(self):
         class ValidClass:
             pass
 
         with pytest.raises(DecoratorException, match="constructor_index must be an int."):
-            inject_mapped_dependency("not-an-int", ["token1"])(ValidClass)
-
-    def test_qualifier_tokens_not_list(self):
-        class ValidClass:
-            pass
-
-        with pytest.raises(DecoratorException, match="qualifier_tokens must be a list."):
-            inject_mapped_dependency(0, "not-a-list")(ValidClass)
+            inject_mapped_dependency("not-an-int", "token1")(ValidClass)
 
     def test_applies_multiple_mapped_dependencies(self):
         class ValidClass:
             pass
 
-        decorated_class = inject_mapped_dependency(0, ["token1"])(ValidClass)
-        decorated_class = inject_mapped_dependency(1, ["token2"])(decorated_class)
+        decorated_class = inject_mapped_dependency(0, "token1")(ValidClass)
+        decorated_class = inject_mapped_dependency(1, "token2")(decorated_class)
 
         assert hasattr(decorated_class, "__di_mapped_dependency")
         assert getattr(decorated_class, "__di_mapped_dependency")[0] == ["token1"]
@@ -49,4 +39,4 @@ class TestInjectMappedDependency:
 
     def test_skip_builtin_classes(self):
         with pytest.raises(DecoratorException, match="inject_mapped_dependency is a class decorator."):
-            inject_mapped_dependency(0, ["token"])(int)
+            inject_mapped_dependency(0, "token")(int)
